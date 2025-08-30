26a8794810e73ec3a96c70d754fc40509b354bad
Cycless <105600559+Cycle1337@users.noreply.github.com>
2025-08-30 17:00:40 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,11 +1,10 @@
 ### AnyKernel3 Ramdisk Mod Script
-## KernelSU with SUSFS By Numbersf
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers
+kernel.string=Paradise Kernel by Cycle1337
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -44,40 +43,6 @@ esac
 ui_print "  -> ksu_supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
-# 确定 root 方式
-if [ -d /data/adb/magisk ] || [ -f /sbin/.magisk ]; then
-    ui_print "检测到 Magisk，当前 Root 方式为 Magisk。在此情况下刷写 KSU 内核有很大可能会导致你的设备变砖，是否要继续？"
-    ui_print "Magisk detected, current root method is Magisk. Flashing the KSU kernel in this case may brick your device, do you want to continue?"
-    ui_print "请选择操作："
-    ui_print "Please select an action:"
-    ui_print "音量上键：退出脚本"
-    ui_print "Volume up key: No"
-    ui_print "音量下键：继续安装"
-    ui_print "Volume down button: Yes"
-    key_click=""
-    while [ "$key_click" = "" ]; do
-        key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_VOLUME')
-        sleep 0.2
-    done
-    case "$key_click" in
-        "KEY_VOLUMEUP") 
-            ui_print "您选择了退出脚本"
-            ui_print "Exiting…"
-            exit 0
-            ;;
-        "KEY_VOLUMEDOWN")
-            ui_print "You have chosen to continue the installation"
-            ;;
-        *)
-            ui_print "未知按键，退出脚本"
-            ui_print "Unknown key, exit script"
-            exit 1
-            ;;
-    esac
-fi
-
-ui_print "开始安装内核..."
-ui_print "Power by GitHub@Numbersf(Aq1298&咿云冷雨)"
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" ] || [ -L "/dev/block/by-name/init_boot_a" ]; then
     split_boot
     flash_boot
@@ -86,46 +51,9 @@ else
     write_boot
 fi
 
-# 优先选择模块路径
-if [ -f "$AKHOME/ksu_module_susfs_1.5.2+_Release.zip" ]; then
-    MODULE_PATH="$AKHOME/ksu_module_susfs_1.5.2+_Release.zip"
-    ui_print "  -> Installing SUSFS Module from Release"
-elif [ -f "$AKHOME/ksu_module_susfs_1.5.2+_CI.zip" ]; then
-    MODULE_PATH="$AKHOME/ksu_module_susfs_1.5.2+_CI.zip"
-    ui_print "  -> Installing SUSFS Module from CI"
-else
-    ui_print "  -> No SUSFS Module found,Installing SUSFS Module from NON,Skipping Installation"
-    MODULE_PATH=""
-fi
+ui_print "Paradise Kernel by Cycle1337"
+ui_print "Telegram: t.me/Cycle1337"
+ui_print "Features: KPM, Fengchi Kernel(if available), O2, lz4kd/lz4 zram"
 
-# 安装 SUSFS 模块（可选）
-if [ -n "$MODULE_PATH" ]; then
-    KSUD_PATH="/data/adb/ksud"
-    ui_print "安装 SUSFS 模块?"
-    ui_print "音量上跳过安装；音量下安装模块"
-    ui_print "Install susfs4ksu Module?"
-    ui_print "Volume UP: NO；Volume DOWN: YES"
-
-    key_click=""
-    while [ "$key_click" = "" ]; do
-        key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_VOLUME')
-        sleep 0.2
-    done
-    case "$key_click" in
-        "KEY_VOLUMEDOWN")
-            if [ -f "$KSUD_PATH" ]; then
-                ui_print "Installing SUSFS Module..."
-                /data/adb/ksud module install "$MODULE_PATH"
-                ui_print "Installation Complete"
-            else
-                ui_print "KSUD Not Found, Skipping Installation"
-            fi
-            ;;
-        "KEY_VOLUMEUP")
-            ui_print "Skipping SUSFS Module Installation"
-            ;;
-        *)
-            ui_print "Unknown Key Input, Skipping Installation"
-            ;;
-    esac
-fi
+sleep 3
+am start -a android.intent.action.VIEW -d tg://resolve?domain=Cycle1337 >/dev/null 2>&1
